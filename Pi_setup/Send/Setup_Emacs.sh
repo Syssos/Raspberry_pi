b0decfae79af8ea1b444aecbad09beaf03507a3c
Syssos <Syssos@gitgub.com>
2018-08-07 13:10:15 -0700

    Folder containing scripts to sent to pi

---
+++ Pi_setup/Send/Setup_Emacs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script installs bash
+sudo apt-get update -y
+sudo apt-get install emacs -y
+. ~/.bashrc
---
+++ Pi_setup/Send/nginx_conf.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#set's up nginx
+sudo apt-get update
+sudo apt-get install nginx
+sudo nginx -v
