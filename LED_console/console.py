dd26a5de88896e52c6c21aa83c36779319dc71f2
Syssos <paral.cody98@gmail.com>
2018-08-18 11:57:34 +0000

    Start of LED test console

--- LED_console/console.py
+++ LED_console/console.py
@@ -2,20 +2,45 @@
 import cmd
 from Colors import colors
 from time import sleep
-
+from gpiozero import LED
 
 class LEDcontrol(cmd.Cmd):
 
     prompt = ("LED ~:$ ")
-    
-    def do_yellowblink(self, args):
-        yel = colors.yellow()
-        while True:
-            colors.turn_on(yel)
-            sleep(1)
-            colors.turn_off(yel)
-            sleep(1)
-        return 1
+    __light = LED(0)
+
+    def do_quit(self, args):
+        ''' Quit command to exit the program.
+        '''
+        return True
+
+    def do_EOF(self, args):
+        ''' Exits after receiving the EOF signal.
+        '''
+        return True
+
+    def emptyline(self):
+        ''' Prevents printing anything when an empty line is passed.
+        '''
+        pass
+
+    def handler(self, signum, frame):
+        # self.emptyline() does not work here
+        # return cmd.Cmd.emptyline(self) does not work here
+        print ("caught ctrl+c, press return to continue")
+
+    def do_select(self, args):
+       self.__light = colors.getcolor(self, args)
+
+    def do_power(self, args):
+        if args is None:
+            print ("please enter an option")
+        else:
+            colors.turn_on_or_off(self.__light, args)
+
+    def do_blink(self, args):
+        colors.flash(self.__light)
+
 
 if __name__ == "__main__":
     LEDcontrol().cmdloop()
