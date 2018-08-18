0bd6f79a4059f5073016ddab1965719612c41374
Syssos <paral.cody98@gmail.com>
2018-08-18 23:22:15 +0000

    updated for error handling

--- LED_console/console.py
+++ LED_console/console.py
@@ -2,13 +2,14 @@
 import cmd
 from models.Colors import colors
 from time import sleep
-from gpiozero import LED
+from gpiozero import *
 
 class LEDcontrol(cmd.Cmd):
 
     prompt = ("LED ~:$ ")
     __light = LED(0)
-
+    __color = ""
+    
     def do_quit(self, args):
         ''' Quit command to exit the program.
         '''
@@ -26,7 +27,11 @@ class LEDcontrol(cmd.Cmd):
         pass
 
     def do_select(self, args):
-       self.__light = colors.getcolor(self, args)
+        if (self.__color == args):
+            print ("color already selected")
+        else:
+            self.__light = colors.getcolor(self, args)
+        self.__color = args
 
     def do_power(self, args):
         if args is None:
@@ -37,6 +42,30 @@ class LEDcontrol(cmd.Cmd):
     def do_blink(self, args):
         colors.flash(self.__light)
 
+    def default(self, args):
+        ''' Catches all the function names that are not expicitly defined.
+        '''
+        functions = {"select": self.do_select, "power": self.do_power,
+                     "blink": self.do_blink}
+        args = (args.replace("(", ".").replace(")", ".")
+                .replace('"', "").replace(",", "").split("."))
+        
+        try:
+            cmd_arg = args[0] + " " + args[2]
+            func = functions[args[1]]
+            func(cmd_arg)
+        except BaseException:
+            print("Unknown LED cmd:", args[0])
 
+    def do_internet(self, args):
+        test = colors.internet_on()
+        if (test == False):
+            pass
+        else:
+            colors.flash(self.__light)
+
+    def do_stop(self, args):
+        colors.execstop(self.__light)
+        
 if __name__ == "__main__":
     LEDcontrol().cmdloop()
